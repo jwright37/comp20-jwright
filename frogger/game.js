4c9b513216c58ed100b2ab6d112876d58e18fb42
John Wright <John.Wright@tufts.edu>
2013-02-12 13:14:01 -0500

    asdf

---
+++ frogger/game.js
@@ -0,0 +1,23 @@
+// Assignment 2 : Frogger
+// John Wright
+// 2.11.2012
+
+function start_game(){
+
+}
+
+function draw(){
+
+}
+
+function initialize(){
+//set le vars
+}
+
+function cheats(){
+//alt music
+//missiles
+//moar lives
+}
+
+
