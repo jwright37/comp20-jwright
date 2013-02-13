8688cb397003e6cb3a155ca622634ea7f2a63461
John Wright <John.Wright@tufts.edu>
2013-02-13 16:11:11 -0500

    finished lab6

--- mspacman/README.txt
+++ mspacman/README.txt
@@ -1,1 +1,8 @@
-asdf
+README for Lab 6
+Ms Pacman
+2/13/13
+
+This lab was very practical since it is so similar to the frogger assignment.
+I found the sprites on the .png image by guessing and checking, which was annoying. 
+I spent about 20 min on this lab and did not give or recieve any help.
+
--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,1 +1,15 @@
-asd
+//javascript file for lab 6
+
+function draw(){
+c = document.getElementById("board");
+ctxt = c.getContext("2d");
+
+img = new Image();
+img.src = "pacman10-hp-sprite.png";
+//background
+ctxt.drawImage(img,320,0,465,138,0,0,465,138);
+//ms pacman
+ctxt.drawImage(img,80,0,20,20,280,76,20,20);
+//ghost (pinky)
+ctxt.drawImage(img,80,100,20,20,280,50,20,20);
+}
