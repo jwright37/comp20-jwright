ca486e0075939fb732308db29ed65de53eac5aa2
John Wright <John.Wright@tufts.edu>
2013-03-03 21:58:47 -0500

    placed carmen and waldo with icons, placed red line stations, fixed funny error in main.js, got it all to render on my site

--- where/where.js
+++ where/where.js
@@ -3,20 +3,23 @@
 	//find,parse waldo
 	//place the red line with fancy markers
 
-google.maps.event.addDomListener(window, 'load', load());	
+google.maps.event.addDomListener(window, 'load', load);	
 	
 //manages program flow...
 function load()
 {
 	draw();
-	//findwaldo();
+	findwaldo();
+	markstations();
 }
 
 //draws the map
 var map;
 function draw()
 {
+	var roughcenter = new google.maps.LatLng(42,-71);
 	var mapOptions = {
+		center: roughcenter,
 		zoom: 13,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
@@ -39,9 +42,8 @@ function draw()
 			});
 		});
 	}
-	//makestations();
 }
-/*
+
 //get location of waldo and carmen
 function findwaldo() {
 	var request = new XMLHttpRequest();
@@ -57,7 +59,8 @@ function findwaldo() {
 					var wmark = new google.maps.Marker({
 						position: wlatlng,
 						map: map,
-						title: list[i].loc.note
+						title: list[i].loc.note,
+						icon: 'waldo.png'
 					});
 				}
 				if(list[i].name == "Carmen Sandiego"){
@@ -65,53 +68,51 @@ function findwaldo() {
 					var wmark = new google.maps.Marker({
 						position: cslatlng,
 						map: map,
-						title: list[i].loc.note
+						title: list[i].loc.note,
+						icon: 'carmen.png'
 					});
 				}
 			}
 	}
 	catch (error){
-		alert("No Waldo");
+		
 	}
 }
-*/
 
-
-//gonna hardcode the station json in (at least for now)
-function makestations (){
-	stnarray = [
-		{"station":"Alewife","lat":42.395428,"lng":-71.142483}
-		{"station":"Davis","lat":42.39674,"lng":-71.121815}
-		{"station":"Porter","lat":42.3884,"lng":-71.119149}
-		{"station":"Harvard","lat":42.373362,"lng":-71.118956}
-		{"station":"Central","lat":42.365486,"lng":-71.103802}
-		{"station":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653}
-		{"station":"Charles/MGH","lat":42.361166,"lng":-71.070628}
-		{"station":"Part St.","lat":42.35639457,"lng":-71.0624242}
-		{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225}
-		{"station":"South","lat":42.352271,"lng":-71.055242}
-		{"station":"Broadway","lat":42.342622,"lng":-71.056967}
-		{"station":"Andrew","lat":42.330154,"lng":-71.057655}
-		{"station":"JFK/UMass","lat":42.320685,"lng":-71.052391}
-		{"station":"Savin Hill","lat":42.31129,"lng":-71.053331}
-		{"station":"Fields Corner","lat":42.300093,"lng":-71.061667}
-		{"station":"Shawmut","lat":42.29312583,"lng":-71.06573796}
-		{"station":"Ashmont","lat":42.284652,"lng":-71.064489}
-		{"station":"North Quincy","lat":42.275275,"lng":-71.029583}
-		{"station":"Wollaston","lat":42.2665139,"lng":-71.0203369}
-		{"station":"Quincy Center","lat":42.251809,"lng":-71.005409}
-		{"station":"Quincy Adams","lat":42.233391,"lng":-71.007153}
+//gonna hardcode the station json in
+function markstations() {
+	var stnarray = [
+		{"station":"Alewife","lat":42.395428,"lng":-71.142483},
+		{"station":"Davis","lat":42.39674,"lng":-71.121815},
+		{"station":"Porter","lat":42.3884,"lng":-71.119149},
+		{"station":"Harvard","lat":42.373362,"lng":-71.118956},
+		{"station":"Central","lat":42.365486,"lng":-71.103802},
+		{"station":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653},
+		{"station":"Charles/MGH","lat":42.361166,"lng":-71.070628},
+		{"station":"Part St.","lat":42.35639457,"lng":-71.0624242},
+		{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225},
+		{"station":"South","lat":42.352271,"lng":-71.055242},
+		{"station":"Broadway","lat":42.342622,"lng":-71.056967},
+		{"station":"Andrew","lat":42.330154,"lng":-71.057655},
+		{"station":"JFK/UMass","lat":42.320685,"lng":-71.052391},
+		{"station":"Savin Hill","lat":42.31129,"lng":-71.053331},
+		{"station":"Fields Corner","lat":42.300093,"lng":-71.061667},
+		{"station":"Shawmut","lat":42.29312583,"lng":-71.06573796},
+		{"station":"Ashmont","lat":42.284652,"lng":-71.064489},
+		{"station":"North Quincy","lat":42.275275,"lng":-71.029583},
+		{"station":"Wollaston","lat":42.2665139,"lng":-71.0203369},
+		{"station":"Quincy Center","lat":42.251809,"lng":-71.005409},
+		{"station":"Quincy Adams","lat":42.233391,"lng":-71.007153},
 		{"station":"Braintree","lat":42.2078543,"lng":-71.0011385}];
 
+	for(i=0;i<stnarray.length;i++){
+		var latlng = new google.maps.LatLng(stnarray[i].lat,stnarray[i].lng);
+		var stnmark = new google.maps.Marker({
+			position: latlng,
+			map: map,
+			title: stnarray[i].station
+		});
+		stnmark.setMap(map);
+		//make info window
+	}
 }
-
-function stationmark(station){
-	var latlng = new google.maps.LatLng(station.stop_lat,station.stop_lng);
-	var marker = new google.maps.Marker({
-		postition: latlng,
-		map: map,
-		//icon!
-		title: station.StationName,
-	});
-}
-*/
