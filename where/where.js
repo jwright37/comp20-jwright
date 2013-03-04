52c3e85bf732a305f13d9f4af6541aac3e47fa9a
John Wright <John.Wright@tufts.edu>
2013-03-04 14:12:39 -0500

    working on scope

--- where/where.js
+++ where/where.js
@@ -3,18 +3,25 @@
 	//find,parse waldo
 	//place the red line with fancy markers
 
+//onload listener
 google.maps.event.addDomListener(window, 'load', load);	
-	
+
+//global variables
+var map;
+var mypos;
+var stnarray = [];
+
 //manages program flow...
 function load()
 {
 	draw();
 	findwaldo();
-	markstations();
+	makestations();
+	makepoly(stnarray);
+	placeme();
 }
 
-//draws the map
-var map;
+//draw the map
 function draw()
 {
 	var roughcenter = new google.maps.LatLng(42,-71);
@@ -24,17 +31,25 @@ function draw()
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-	
+}
+
 //get my location
+function placeme() {
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			var mypos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			var infowindow = new google.maps.InfoWindow({
+			mypos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			map.setCenter(mypos);
+			
+			var dist = closeststn();
+			var myinfowindow = new google.maps.InfoWindow({
 				map: map,
 				position: mypos,
-				content: 'I am here at ' + mypos.lat() + ', ' + mypos.lng()
 			});
-			map.setCenter(mypos);
+			var contentstr = 'I am here at ' + mypos.lat ', ' +mypos.lng
+							+ '<br>The closest T stop is ' + stnarray[dist[0]].name
+							+ '<br>The station is ' + dist[1] + 'miles away.';
+			myinfowindow.setContent(contentstr);
+			
 			var mymark = new google.maps.Marker({
 				position: mypos,
 				map: map,
@@ -45,6 +60,7 @@ function draw()
 }
 
 //get location of waldo and carmen
+//and list distance in infowindow
 function findwaldo() {
 	var request = new XMLHttpRequest();
 	
@@ -53,6 +69,7 @@ function findwaldo() {
 			request.send();
 			var str = request.responseText;
 			var list = JSON.parse(str);
+			//mark their locations
 			for (i=0;i<list.length;i++){
 				if(list[i].name == "Waldo"){
 					var wlatlng = new google.maps.LatLng(list[i].loc.latitude,list[i].loc.longitude);
@@ -62,26 +79,42 @@ function findwaldo() {
 						title: list[i].loc.note,
 						icon: 'waldo.png'
 					});
+					google.maps.event.addListener(wmark, 'click', function() {
+						var dist = calcdist(mypos, wlatlng);
+						var winfo = new google.maps.InfoWindow({
+							position: wlatlng,
+							map: map,
+							content: 'Waldo is ' + dist + 'miles away.'
+						});
+					}
 				}
 				if(list[i].name == "Carmen Sandiego"){
 					var cslatlng = new google.maps.LatLng(list[i].loc.latitude,list[i].loc.longitude);
-					var wmark = new google.maps.Marker({
+					var csmark = new google.maps.Marker({
 						position: cslatlng,
 						map: map,
 						title: list[i].loc.note,
 						icon: 'carmen.png'
 					});
+					google.maps.event.addListener(csmark, 'click', function() {
+						var dist = calcdist(mypos, cslatlng);
+						var csinfo = new google.maps.InfoWindow({
+							position: cslatlng,
+							map: map,
+							content: 'Carmen is ' + dist + 'miles away.'
+						});
+					}
 				}
 			}
 	}
 	catch (error){
-		
+		//do something here
 	}
 }
 
 //gonna hardcode the station json in
-function markstations() {
-	var stnarray = [
+function makestations() {
+	stnarray = [
 		{"station":"Alewife","lat":42.395428,"lng":-71.142483},
 		{"station":"Davis","lat":42.39674,"lng":-71.121815},
 		{"station":"Porter","lat":42.3884,"lng":-71.119149},
@@ -110,13 +143,12 @@ function markstations() {
 		var stnmark = new google.maps.Marker({
 			position: latlng,
 			map: map,
-			title: stnarray[i].station
+			title: stnarray[i].station,
+			icon: 'thomas.jpg'
 		});
 		stnmark.setMap(map);
 		//make info window
 	}
-	
-	makepoly(stnarray);
 }
 
 //polyline
@@ -128,8 +160,8 @@ function makepoly(array) {
 		mainarr[i] = new google.maps.LatLng(array[i].lat, array[i].lng);
 	}
 	
-	for(i=1;i<5;i++){
-		brancharr[i] = new google.maps.LatLng(array[i+17].lat, array[i+17].lng);
+	for(i=1;i<6;i++){
+		brancharr[i] = new google.maps.LatLng(array[i+16].lat, array[i+16].lng);
 	}
 	brancharr[0] = new google.maps.LatLng(array[13].lat, array[13].lng);
 	
@@ -151,6 +183,41 @@ function makepoly(array) {
 	branchpoly.setMap(map);
 }
 
+//calculate the distance between two points using the haversine formula
+function calcdist(coord1, coord2){
+	var lat1 = coord1.lat;
+	var lat2 = coord2.lat;
+	var lng1 = coord1.lng;
+	var lng2 = coord2.lng;
+	var R = 3963.1676; //miles
+	
+	var dLat = (lat2-lat1).toRad();
+	var dLng = (lng2-lng1).toRad();
+	var lat1 = lat1.toRad();
+	var lat2 = lat2.toRad();
+
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLng/2) * Math.sin(dLng/2) * Math.cos(lat1) * Math.cos(lat2); 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c;
+	return d;
+}
+
+function closeststn() {
+	var mindist = 1000;
+	var stncoord;
+	var minindex = -1;
+	
+	for(i=0;i<stnarray.length;i++){
+		stncoord = new google.maps.LatLng(stnarray[i].lat, stnarray[i].lng);
+		var dist = calcdist(mypos,stncoord);
+		if(dist < mindist){
+			mindist = dist;
+			minindex = i;
+		}
+	}
+	return [minindex, mindist];
+}
 //make schedule info boxes
 /*function getsched() {
 	var schedrqst = new XMLHttpRequest();
