b96e60a417ae965cb690d01631ab3a72eb018e6e
John Wright <John.Wright@tufts.edu>
2013-04-14 15:15:46 -0400

    added requirements and comments to web.js

--- scorecenter/web.js
+++ scorecenter/web.js
@@ -2,20 +2,33 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.sendfile('/index.html');	//send a full page
+//index of web app
+//lists all scores for all games
+app.get('/', function(req, res) {
+  res.sendfile('index.html');	//send a full page
 });
 
-app.get('/scores', function(request, response) {
-  response.send('highscore is over 9000!');
+//username search
+//searches for all the scores for a specific username
+app.get('/usersearch'), function(req, res) {
+	res.sendfile('usersearch.html')  //stuff
 });
 
-/* add /sumbit.json to mondo
-app.post('/scores', function(request, response) {
-  response.send('highscore is over 9000!');
+//get API
+//returns the top ten highscores
+//has game_title as parameter
+app.get('/highscores.json', function(req, res) {
+  res.send('highscore is over 9000!');
 });
-*/
 
+//post API
+//adds a score to the database
+//cross origin resourse sharing?
+app.post('/submit.json', function(req, res) {
+  res.send('highscore is over 9000!');
+});
+
+//listen
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
